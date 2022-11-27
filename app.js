dc306c2498dd9db2b67d9a393c93fa9991ae12b3
kacemrayen13@gmail.com <kacemrayen13@gmail.com>
2022-11-27 12:27:07 +0000

    jenkins

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const parkings = require('./parkings.json')
+
+app.get('/parkings', function (req, res) {
+
+  res.status(200).json(parkings)
+})
+
+/*
+app.get('/parkings/:id', function (req, res) {
+    const id = parseInt(req.params.id)
+    const parking = parkings.find(parking => parking.id == id)
+    res.status(200).json(parking)
+  })
+
+app.post('/parkings', function (req,res) {
+    parkings.push(req.body)
+    res.status(200).json(parkings)
+  })
+*/
+
+app.get('/',function(req,res) {
+  res.send("Hello World");
+});
+
+app.use(express.json())
+app.listen(3000)
